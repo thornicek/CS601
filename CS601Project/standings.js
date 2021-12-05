606a2aae80d9f5e39ddff3280f3cc2a65a9a8394
Tomas Hornicek <tomas@Tomass-MacBook-Pro.local>
2021-12-05 17:04:26 -0500

    implemented table js

--- CS601Project/standings.js
+++ CS601Project/standings.js
@@ -1,25 +1,45 @@
-async function loadIntoTable(url, table){
-    const tableHead = table.querySelector("thead");
-    const tableBody = table.querySelector("tbody");
-    const response = await (fetch);
-    const data = await response.json();
-    console.log(data);
-    tableHead.innerHTML = "<tr></tr>";
-    tableBody.innerHTML = "";
-
-    
-
-
-    
+window.addEventListener('DOMContentLoaded', event =>{
+    displayTable();
+});
+const url = "https://statsapi.web.nhl.com/api/v1/standings/byLeague"
+async function MyFetch(url) {
+    let response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`HTTP error! status : ${reponse.status}`);
+    }
+    return await response.json();
 }
-const url = "https://statsapi.web.nhl.com/api/v1/teams/6/stats"
-loadIntoTable(url,document.querySelector("table"));
+function displayTable(){
+    var table = document.getElementById("standings");
+    try{
+        MyFetch(url).then(json_data => {
+            recordsObject = json_data.records[0];
+            //console.log(recordsObject);
+            teamArray = recordsObject.teamRecords;
+            teamArray.forEach(teamObject => {
+             var row = createRow(teamObject);
+             table.append(row);
+             
+            });
+        });
+    }
+    catch(exception){
 
+    }
+}
+function createRow(teamObject){
+    var teamName = teamObject.team.name;
+    var teamWins = teamObject.leagueRecord.wins;
+    var teamLosses = teamObject.leagueRecord.losses;
+    var teamOTLoss = teamObject.leagueRecord.ot;
+    var teamStreak = teamObject.streak.streakCode;
+    var row = document.createElement("tr");
+    //repeat for all var
+    var col1 = document.createElement("th");
+    col1.innerHTML = teamName;
+    row.append(col1);
 
+    return row;
 
-
-
-function getStandingsURL(id) {
-    return `https://statsapi.web.nhl.com/api/v1/teams/${id}/stats`;
 }
 
